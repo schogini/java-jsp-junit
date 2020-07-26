17ffb7ff14aa195e19b0c58401c6e285db2bdcd7
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-26 16:05:41 +0530

    Sree

---
+++ SelTest.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class SelTest {
+    public static void main(String[] args) {
+
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("TEST_URL");
+
+        // Replace INPUT1, INPUT2 & RESULT - during runtime
+        // String t1 = "10";
+        // String t2 = "5";
+        String res = "TEST_RESULT";
+
+
+        // WebElement element1 = driver.findElement(By.name("t1"));
+        // WebElement element2 = driver.findElement(By.name("t2"));
+
+        // element1.sendKeys(t1);
+        // element2.sendKeys(t2);
+        // element1.submit();
+
+        // System.out.println("Page title is: " + driver.getTitle());
+
+        // System.out.println(driver.getPageSource());
+
+        if (driver.getPageSource().contains(res)) {
+          System.out.println("SUCCESS");
+        } else {
+          System.out.println("FAILED");
+        }
+
+        driver.quit();
+    }
+}
---
+++ sel-test-8080.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# BUILD-ID AND IMAGE NAME
+
+# LAUNCH A TOMCAT INSTANCE FOR TETING
+
+docker network create tmp-net > log.txt 2>&1
+docker container rm -f tmp-web >> log.txt 2>&1
+
+docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2 >> log.txt 2>&1
+
+# docker ps
+sleep 20
+
+
+
+
+
+# [ $# -ne 3 ] && echo 'NEED REPO, TEST SITE URL AND RESULTS' && exit 127 
+
+# git clone $1  > /dev/null 2>&1
+# fol=$(echo $1| sed 's/^.*\///'|sed 's/\.git$//')
+# cd $fol
+
+export CLASSPATH=".:/var/jar_repo/htmlunit-driver-2.42.0-jar-with-dependencies.jar:/var/jar_repo/selenium-server-standalone-3.141.59.jar:/var/jar_repo/testng-6.0.1.jar"
+
+# GIT=$(echo $2|sed 's/\//\\\//g')
+# URL=$(echo $3|sed 's/\//\\\//g')
+
+sed -i "s/TEST_URL/http:\/\/tmp-web:8080\/sample\//" SelTest.java
+sed -i "s/TEST_RESULT/App Version: $1/" SelTest.java
+
+javac SelTest.java
+
+output=`java SelTest | grep -c SUCCESS`
+
+# CLEAR
+docker container rm -f tmp-web >> log.txt 2>&1
+
+if [ $output -gt 0 ] 
+then
+  echo 'SUCCESS'
+  exit 0
+else
+  echo 'FAILED'
+  exit 127
+fi
