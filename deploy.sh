b4e1206924a783d9b83699d89b2c4fce3e4a5089
Sreeprakash Neelkantan <sree@schogini.com>
2021-03-18 11:44:51 +0530

    deploy.sh-port-added

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,9 @@ if [ $qq -gt 0 ]; then
 	docker service update --env-add WEB=$2 --image $1 tmp-svc
 else
 	echo "Deploying Application.. $1"
-	docker service create --name tmp-svc --replicas 2 --publish 8080:8080 --env WEB=$2 $1
+        port=8080
+	[ $# -eq 3 ] && port=$3 
+
+	docker service create --name tmp-svc --replicas 2 --publish $port:8080 --env WEB=$2 $1
 fi
+
