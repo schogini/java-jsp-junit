05e6e755fbf2cdb8c000464f4ed21599825d16ed
Sreeprakash Neelkantan <sree@schogini.com>
2021-03-18 11:54:09 +0530

    deploy.sh-port-added

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
+# IMAGE BUILD_ID PORT SERVICE_NAME
 
 qq=$(docker service ls|grep -c tmp-svc)
 
+service_name=tmp-svc
+[ $# -gt 3 ] && service_name=$4 
+
+port=8080
+[ $# -gt 2 ] && port=$3 
+
 if [ $qq -gt 0 ]; then
 	echo "Doing a Rolling Update.. $1 "
-	docker service update --env-add WEB=$2 --image $1 tmp-svc
+	docker service update --env-add WEB=$2 --image $1 $service_name
 else
-	echo "Deploying Application.. $1"
-        port=8080
-	[ $# -eq 3 ] && port=$3 
+	echo "Deploying Application.. service-name=$service_name port=$port build=$2 image=$1"
 
-	docker service create --name tmp-svc --replicas 2 --publish $port:8080 --env WEB=$2 $1
+	docker service create --name $service_name --replicas 2 --publish $port:8080 --env WEB=$2 $1
 fi
 
